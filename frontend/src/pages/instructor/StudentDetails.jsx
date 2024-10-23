3d3f082ec5de4b1b9212c7d890d342c2c712b363
Sinclair <nsincl01@ead.ubc.ca>
2024-10-23 14:00:11 -0700

    updated studentview on instruct view

--- frontend/src/pages/instructor/StudentDetails.jsx
+++ frontend/src/pages/instructor/StudentDetails.jsx
@@ -2,19 +2,29 @@ import { useEffect, useState, useRef } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import { fetchAuthSession, fetchUserAttributes } from "aws-amplify/auth";
 import { toast, ToastContainer } from "react-toastify";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import "react-toastify/dist/ReactToastify.css";
 import PageContainer from "../Container";
 import {
   Box,
   Typography,
   Divider,
-  Switch,
   TextField,
   Button,
   Paper,
-  IconButton
+  IconButton,
+  Tabs,
+  Tab,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
 } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const handleBackClick = () => {
   window.history.back();
@@ -24,7 +34,7 @@ const formatMessages = (messages) => {
   // Helper function to format date as YY/MM/DD
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    if (isNaN(date.getTime())) return "Invalid Date"; 
+    if (isNaN(date.getTime())) return "Invalid Date";
 
     return date
       .toLocaleDateString(undefined, {
@@ -66,15 +76,18 @@ const StudentDetails = () => {
   const { studentId } = useParams();
   const location = useLocation();
   const { simulation_group_id, student } = location.state;
-  const [unenroll, setUnenroll] = useState(false);
+  const [tabs, setTabs] = useState([]);
+  const [sessions, setSessions] = useState({});
+  const [activeTab, setActiveTab] = useState(0);
   const [chatHistory, setChatHistory] = useState(`loading...`);
+  const [dialogOpen, setDialogOpen] = useState(false);
   const textFieldRef = useRef(null);
 
   useEffect(() => {
     const fetchHistory = async () => {
       try {
         const session = await fetchAuthSession();
-        const token = session.tokens.idToken
+        const token = session.tokens.idToken;
         const response = await fetch(
           `${
             import.meta.env.VITE_API_ENDPOINT
@@ -91,10 +104,10 @@ const StudentDetails = () => {
         );
         if (response.ok) {
           const data = await response.json();
-          const formattedMessages = formatMessages(data);
-          setChatHistory(formattedMessages);
+          setSessions(data);
+          setTabs(Object.keys(data));
         } else {
-          console.error("Failed to fetch students:", response.statusText);
+          console.error("Failed to fetch student data:", response.statusText);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -102,39 +115,26 @@ const StudentDetails = () => {
     };
 
     fetchHistory();
-  }, [simulation_group_id, student.email]); 
+  }, [simulation_group_id, student.email]);
 
   useEffect(() => {
     if (textFieldRef.current) {
       textFieldRef.current.scrollTop = textFieldRef.current.scrollHeight;
     }
-  }, [chatHistory]); 
+  }, [chatHistory]);
 
-  const handleToggle = () => {
-    setUnenroll(!unenroll);
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
   };
 
-  const handleUnenroll = () => {
-    if (unenroll) {
-      deleteStudent();
-    } else {
-      toast.success("Saved Successfully", {
-        position: "top-center",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
-    }
+  const handleDialogClose = () => {
+    setDialogOpen(false);
   };
 
-  const deleteStudent = async () => {
+  const handleUnenroll = async () => {
     try {
       const session = await fetchAuthSession();
-      const token = session.tokens.idToken
+      const token = session.tokens.idToken;
       const { email } = await fetchUserAttributes();
       const response = await fetch(
         `${
@@ -163,11 +163,10 @@ const StudentDetails = () => {
           progress: undefined,
           theme: "colored",
         });
-        setTimeout(function () {
+        setTimeout(() => {
           handleBackClick();
         }, 1000);
       } else {
-        console.error("Failed to fetch students:", response.statusText);
         toast.error("Failed to unenroll student", {
           position: "top-center",
           autoClose: 1000,
@@ -178,6 +177,7 @@ const StudentDetails = () => {
           progress: undefined,
           theme: "colored",
         });
+        console.error("Failed to unenroll student:", response.statusText);
       }
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -198,56 +198,110 @@ const StudentDetails = () => {
           sx={{
             width: "100%",
             overflow: "auto",
-            marginTop: 4,
             padding: 2,
             overflowY: "scroll",
+            marginTop: 4,
           }}
         >
           <Box mb={2} sx={{ flexGrow: 1, p: 3, textAlign: "left", mt: 6 }}>
             <Typography variant="h5">Student Name: {studentId}</Typography>
             <Divider sx={{ my: 2 }} />
             <Typography variant="body1">Email: {student.email}</Typography>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <Typography variant="body1">Unenroll from Simulation Group:</Typography>
-              <Switch checked={unenroll} onChange={handleToggle} />
-            </Box>
-            <TextField
-              label="Chat History"
-              variant="outlined"
-              fullWidth
-              multiline
-              rows={10}
-              value={chatHistory}
-              InputProps={{
-                readOnly: true,
-              }}
-              sx={{ my: 2 }}
-              inputRef={textFieldRef} 
-            />
+
             <Button
-              onClick={() => {
-                handleUnenroll();
-              }}
+              onClick={handleDialogOpen} // Open dialog on button click
+              sx={{ marginBottom: 6 }}
               variant="contained"
               color="primary"
             >
-              Save
+              Unenroll Student
             </Button>
+
+            <Dialog
+              open={dialogOpen}
+              onClose={handleDialogClose}
+              aria-labelledby="confirm-unenroll-dialog"
+            >
+              <DialogTitle id="confirm-unenroll-dialog">
+                Confirm Unenroll
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Are you sure you want to unenroll the student from this
+                  course?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleDialogClose} color="primary">
+                  Cancel
+                </Button>
+                <Button
+                  onClick={() => {
+                    handleDialogClose();
+                    handleUnenroll(); // Call unenroll function on confirm
+                  }}
+                  color="error"
+                >
+                  Confirm
+                </Button>
+              </DialogActions>
+            </Dialog>
+
+            <Typography variant="h5" sx={{ mb: 2 }}>
+              Chat History:
+            </Typography>
+
+            <Box
+              sx={{
+                borderBottom: 1,
+                borderColor: "divider",
+                overflowX: "auto",
+              }}
+            >
+              <Tabs
+                value={activeTab}
+                onChange={(_, newValue) => setActiveTab(newValue)}
+                variant="scrollable"
+                scrollButtons="auto"
+              >
+                {tabs.map((tabName, index) => (
+                  <Tab key={index} label={tabName} />
+                ))}
+              </Tabs>
+            </Box>
+
+            {sessions[tabs[activeTab]]?.length > 0 ? (
+              sessions[tabs[activeTab]].map((session, index) => (
+                <Accordion key={index}>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>{session.sessionName}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <TextField
+                      label="Chat History"
+                      variant="outlined"
+                      fullWidth
+                      multiline
+                      rows={10}
+                      value={formatMessages(session.messages)}
+                      InputProps={{
+                        readOnly: true,
+                      }}
+                      sx={{ my: 2 }}
+                      inputRef={textFieldRef}
+                    />
+                  </AccordionDetails>
+                </Accordion>
+              ))
+            ) : (
+              <Typography sx={{ ml: 2, mt: 4 }} variant="body1">
+                Student has not entered the module yet.
+              </Typography>
+            )}
           </Box>
         </Paper>
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
       </PageContainer>
+      <ToastContainer />
     </>
   );
 };
