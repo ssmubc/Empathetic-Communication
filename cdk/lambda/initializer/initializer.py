b5cb798f77c4092fa363e423d86c9a18afb6ed44
Aman <apraka01@student.ubc.ca>
2025-02-23 18:27:27 -0800

    Modified patient_data table to keep track of whether the document has been ingested or not.

--- cdk/lambda/initializer/initializer.py
+++ cdk/lambda/initializer/initializer.py
@@ -99,7 +99,8 @@ def handler(event, context):
                 "filename" varchar,
                 "time_uploaded" timestamp,
                 "metadata" text,
-                "file_number" integer
+                "file_number" integer,
+                "ingestion_status" BOOLEAN DEFAULT FALSE
             );
 
             CREATE TABLE IF NOT EXISTS "student_interactions" (
